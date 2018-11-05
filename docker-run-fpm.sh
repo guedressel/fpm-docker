0f3d1b52986a297786b00198411a6b2ee1325df3
guedressel <guedressel@users.noreply.github.com>
2018-11-05 22:50:15 +0100

    Dynamically add pseudo-tty allocation

--- docker-run-fpm.sh
+++ docker-run-fpm.sh
@@ -6,4 +6,13 @@ TAG=latest
 USER=`id -u`
 GROUP=`id -g`
 
-exec docker run --rm -i -g $USER:$GROUP -v $PWD:/build $IMG:$TAG "$@"
+if [ -t 1 ]
+then
+  # running in a (pseudo-)tty
+  interact=-it
+else
+  # no tty around
+  interact=-i
+fi
+
+exec docker run --rm $interact -g $USER:$GROUP -v $PWD:/build $IMG:$TAG "$@"
